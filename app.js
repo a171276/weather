dd2604be42f9d366a19ff637304323dd1100813b
alanph <63068838+alanph@users.noreply.github.com>
2020-04-14 18:40:52 +0100

    Add wf to pick up weather forecast and other bits

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const express = require('express');
+const path = require('path');
+const fetch = require('node-fetch');
 
 const app = express();
 
@@ -17,9 +19,34 @@ const credentials = {
 	ca: ca
 };
 
-app.use((req, res) => {
-	res.send('Hello there !');
+app.get('/wtf', function (req, res) {
+    let locations = ['352605', '324166', '322954', '353370', '324249', '354677'];
+    let apiKey = 'd5df697d-3ed8-4efd-94ed-c67283bb23ee';
+   
+    locations.forEach(location => {
+        let url = `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${location}?res=3hourly&key=${apiKey}`;
+        fetch(url)
+        .then(res => res.json())
+        .then(json => {
+            let fn = path.join(__dirname, 'html') + '/json/' + location + '.json';
+            let data = JSON.stringify(json)
+            fs.writeFile(fn, data, function (err) {
+            if (err) return console.log(err);
+            console.log(`Written ${location}`);
+            });
+        });
+    });
+    res.send('done');
 });
+
+
+app.use(express.static(path.join(__dirname, 'html')))
+
+//app.use((req, res) => {
+//	res.send('Hello there !');
+//});
+
+
 
 // Starting both http & https servers
 const httpServer = http.createServer(app);
